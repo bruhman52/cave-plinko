54a88d723681cdc59c590f4232740d4553266c34
Josh <joshmcgraw2009@gmail.com>
2026-02-21 18:21:03 -0600

    backend stuff

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ peg_surf = pg.image.load("peg.png").convert_alpha()
 peg_surf = pg.transform.scale(peg_surf, (40, 40))
 peg_surf.set_colorkey((255,0,255))
 
-# ball setup
+# player setup
+at_rest = True
 mass = 2
 radius = 35
 moment = pm.moment_for_circle(mass, 0, radius)
@@ -61,8 +62,10 @@ all_borders = []
 
 horizontal_spacing = 250 
 vertical_spacing = horizontal_spacing * 0.866 
-last_generated_y = 200 
-rows_generated = 0
+last_generated_y_bottom = 200
+last_generated_y_top = 200
+rows_gen_bottom = 0
+rows_gen_top = 0
 grid_width = 0
 
 def spawn_peg_row(y_pos, row_index, cols=15):
@@ -77,6 +80,23 @@ def spawn_peg_row(y_pos, row_index, cols=15):
         
         new_peg = Peg(space, x, y_pos)
         all_pegs.add(new_peg)
+
+# reset function
+def reset():
+    global last_generated_y_bottom, last_generated_y_top, rows_gen_bottom, rows_gen_top
+    player_body.position = (screen.get_width() / 2, -200) #also change in game loop
+    player_body.velocity = (0, 0)
+
+    for peg in all_pegs:
+        space.remove(peg.shape, peg.body)
+        peg.kill()
+
+    last_generated_y_bottom = 200
+    last_generated_y_top = 200
+    rows_gen_bottom = 0
+    rows_gen_top = 0
+    global at_rest
+    at_rest = True
 
 
 # camera class
@@ -97,29 +117,49 @@ class Camera():
                 pm.Transform.translation(-self.x, -self.y))
     
 cam = Camera(screen.get_width(), screen.get_height())
-cam.zoom = 0.5
+cam.zoom = 0.3
 cam.x = screen.get_width() / 2
 cam.y = screen.get_height() / 2
 debug_cam_toggle = False
+
+"""
+
+GAME LOOP
+
+"""
+
+reset()
 
 while running:
     # to exit (idk why this isnt already just a thing wtf guys)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-        if event.type == pg.KEYDOWN and event.key == pg.K_1:
-            if debug_cam_toggle == False:
-                debug_cam_toggle = True
-            else:
-                debug_cam_toggle = False
+
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_1:
+                if debug_cam_toggle == False:
+                    debug_cam_toggle = True
+                else:
+                    debug_cam_toggle = False
+        if event.type == pg.MOUSEBUTTONDOWN and at_rest:
+            if event.button == 1:  # Left click
+                at_rest = False
+
+
         if event.type == pg.MOUSEWHEEL:
-            cam.zoom += event.y * 0.05
-            cam.zoom = max(0.1, min(cam.zoom, 2.0))
+            cam.zoom += event.y * 0.01
+            cam.zoom = max(0.01, min(cam.zoom, 2.0))
+            print(f"Zoom: {cam.zoom:.2f}")
     # wipe screen
     screen.fill("black")
 
     draw_options.transform = cam.get_pm_transform()
     space.debug_draw(draw_options)
+
+    if at_rest:
+        player_body.position = (screen.get_width() / 2, -200) #also change reset() func
+        player_body.velocity = (0, 0)
 
     dt = clock.tick(60) / 1000
     space.step(dt)
@@ -142,18 +182,28 @@ while running:
         cam.y = player_body.position.y
 
     if keys[pg.K_r]:
-        player_body.position = (screen.get_width() / 2, 0)
-        player_body.velocity = (0,0)
+        reset()
 
     # gen new pegs and delete old ones
-    if player_body.position.y > last_generated_y - 1000:
-        for _ in range(5): 
-            spawn_peg_row(last_generated_y, rows_generated)
-            last_generated_y += vertical_spacing
-            rows_generated += 1
 
+    # gen down
+    if player_body.position.y > last_generated_y_bottom - 2000:
+        for _ in range(5):
+            spawn_peg_row(last_generated_y_bottom, rows_gen_bottom)
+            last_generated_y_bottom += vertical_spacing
+            rows_gen_bottom += 1
+
+    # gen up
+    if player_body.position.y < last_generated_y_top + 2000 and last_generated_y_top > 200:
+        for _ in range(5):
+            rows_gen_top -= 1
+            last_generated_y_top -= vertical_spacing
+            spawn_peg_row(last_generated_y_top, rows_gen_top)
+
+    # cleanup
     for peg in all_pegs:
-        if peg.body.position.y < player_body.position.y - 1500:
+        dist = peg.body.position.y - player_body.position.y
+        if abs(dist) > 4000: 
             space.remove(peg.shape, peg.body)
             peg.kill()
 
@@ -176,7 +226,6 @@ while running:
     # tick player
     if abs(player_body.velocity.x) < 5 or abs(player_body.velocity.y) < 10:
         player_body.apply_impulse_at_local_point((r.randint(-10,10),0))
-
 
     # draw to screen
     pg.display.flip()
